c59fb365681753515f79287162d83f9d5652ad16
Jeff Hajewski <jeff.hajewski@gmail.com>
2017-10-13 21:06:53 -0500

    Adds comment, slightly refactors example

--- rayon-search/src/main.rs
+++ rayon-search/src/main.rs
@@ -8,8 +8,11 @@ error_chain! {}
 
 fn run() -> Result<()> {
     let v = vec![6, 2, 1, 9, 3, 8, 11];
-    let found = v.par_iter().find_any(|&x| x == &9);
-    let not_found = v.par_iter().find_any(|&x| x == &0);
+    // Note: |v.par_iter()| gives us an iterator that references
+    // |v|, and the closure references those elements, thus we have
+    // a reference to a reference of an element.
+    let found = v.par_iter().find_any(|&&x| x == 9);
+    let not_found = v.par_iter().find_any(|&&x| x == 0);
 
     assert!(found.is_some());
     assert!(!not_found.is_some());
