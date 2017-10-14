619d84420877e1c41162846844ac611484c16757
Jeff Hajewski <jeff.hajewski@gmail.com>
2017-10-13 20:59:22 -0500

    Gets minimal example working

--- rayon-search/src/main.rs
+++ rayon-search/src/main.rs
@@ -2,13 +2,17 @@
 extern crate error_chain;
 extern crate rayon;
 
-error_chain! {
-    foreign_links {
+use rayon::prelude::*;
 
-    }
-}
+error_chain! {}
 
 fn run() -> Result<()> {
+    let v = vec![6, 2, 1, 9, 3, 8, 11];
+    let found = v.par_iter().find_any(|&x| x == &9);
+    let not_found = v.par_iter().find_any(|&x| x == &0);
+
+    assert!(found.is_some());
+    assert!(!not_found.is_some());
 
     Ok(())
 }
